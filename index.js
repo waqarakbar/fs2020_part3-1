8b02aad0790d7ed24c9ae7f938c34cb0c226470b
Waqar Akbar <waqarakbar89@gmail.com>
2020-08-21 20:58:53 +0500

    3.5: Phonebook backend step5

--- index.js
+++ index.js
@@ -60,6 +60,19 @@ app.delete('/api/persons/:id', (request, response) => {
 	response.status(204).end()
 })
 
+app.post('/api/persons', (request, response) => {
+
+	const body = request.body 
+	const newPerson = {
+		id: Math.floor(Math.random()*10000),
+		name: body.name,
+		number: body.number
+	}
+	persons = persons.concat(newPerson)
+
+	response.json(newPerson)
+})
+
 const PORT = 3001
 app.listen(PORT)
 console.log(`Server running on port ${PORT}`)
