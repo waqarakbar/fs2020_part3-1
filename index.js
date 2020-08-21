99668b2d7fd8e28f0ffb6744134f4b840626addb
Waqar Akbar <waqarakbar89@gmail.com>
2020-08-21 20:39:40 +0500

    3.3: Phonebook backend step3

--- index.js
+++ index.js
@@ -44,6 +44,7 @@ app.get('/api/persons', (request, response) => {
 app.get('/api/persons/:id', (request, response) => {
 	const id = Number(request.params.id)
 	const person = persons.find(p => p.id === id)
+	
 	if(person){
 		return response.status(200).json(person)
 	}else{
