c86edab55b3e9999d746850a5d7af4ad3b6e2350
Waqar Akbar <waqarakbar89@gmail.com>
2020-08-22 21:54:21 +0500

    3.8*: Phonebook backend step8

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const express = require('express')
 const morgan = require('morgan')
 const app = express()
 
-app.use(morgan('tiny'))
+morgan.token('body', function (req, res) { return JSON.stringify(req.body) })
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms - :body'));
 
 const requestLogger = (request, response, next) => {
 	console.log('Method:', request.method)
