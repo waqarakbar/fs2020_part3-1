dd6475a6bc788162fbac153ea0525cf1b89a6a03
Waqar Akbar <waqarakbar89@gmail.com>
2020-08-24 12:52:03 +0500

    3.9 phonebook backend step9

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
 const app = express()
 
 morgan.token('body', function (req, res) { return JSON.stringify(req.body) })
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms - :body'));
+
+app.use(cors())
 
 const requestLogger = (request, response, next) => {
 	console.log('Method:', request.method)
