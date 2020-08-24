16fde066f557b598d617fcc5cdc7efb9ea211c5d
Waqar Akbar <waqarakbar89@gmail.com>
2020-08-24 13:08:00 +0500

    front end

---
+++ build/precache-manifest.c02f8150785e4d4c824f2f0c13f79348.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79652a6c05526aa7827bc0eadb90a64e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "07ed1aa1ba1e727f9c4f",
+    "url": "/static/css/main.2d3e5882.chunk.css"
+  },
+  {
+    "revision": "c164db0d716c10a5f408",
+    "url": "/static/js/2.f7f5c33f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.f7f5c33f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "07ed1aa1ba1e727f9c4f",
+    "url": "/static/js/main.dbc587a6.chunk.js"
+  },
+  {
+    "revision": "f4d2ac87a76b6331fd91",
+    "url": "/static/js/runtime-main.4234e0d5.js"
+  }
+]);
