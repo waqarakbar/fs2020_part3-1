84fa2beb8374998dee246e4e1635afcc08ea08aa
Waqar Akbar <waqarakbar89@gmail.com>
2020-08-24 12:59:50 +0500

    fixing bugs

--- index.js
+++ index.js
@@ -116,6 +116,7 @@ const unknownEndpoint = (request, response) => {
 }
 app.use(unknownEndpoint)
 
-const PORT = 3001
-app.listen(PORT)
-console.log(`Server running on port ${PORT}`)
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
